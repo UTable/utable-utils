8b9699454470d4948e88bdfce59ce0addc1f8e5b
bachittle <bachittle@gmail.com>
2021-04-25 09:45:17 -0400

    some fixes for production

--- tts_cronjob.sh
+++ tts_cronjob.sh
@@ -2,4 +2,4 @@
 
 # runs python script, and outputs log files to appropriate folder
 
-python3.7 tts_cronjob.py > "local-data/logs/$(date "+%Y-%m-%d"_ttscron.log)"
+./tts_cronjob.py > "local-data/logs/$(date "+%Y-%m-%d"_ttscron.log)"
