6eba2d777252076ed4a259db1a6234d5896f669b
bachittle <bachittle@gmail.com>
2021-04-24 20:15:29 -0400

    adding an entry point for a cron job

--- pdf-mod/script2.sh
+++ pdf-mod/script2.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 #
 # Pretty formatting of the ugly PDF parse by tika
-# gets all the information I need:
-# - course code
-# - course name
-#
-# starting to create the YAML results file. Still need to parse sections
-# will do so in script3.sh
+# still needs more parsing in tts.py
 
-./strip_empty_lines.sh | gawk '
+./strip_empty_lines.sh $1 | gawk '
 	BEGIN {printf "w2021";flag=0} 
 	/^(Section|(M|T|W|TH|F|SA|SU)+ )/ {flag=1;printf "\n  "}  
 	/^[A-Z][A-Z][A-Z][A-Z]-( )*[0-9][0-9X][0-9X][0-9X]/ {
@@ -16,7 +11,7 @@
 		split($0, a, "\\\(-\\\)", sep);
 		printf "\n%s\n  name: \"%s\"", a[1], a[2]
 	}
-	/Winter 2021 Course Offerings/ {flag=0}
+	/Course Offerings/ {flag=0}
 	flag {printf "%s ", $0}
 ' 
 
---
+++ tts_cronjob.py
@@ -0,0 +1,45 @@
+# tts (text to scrape) automation via cron job
+# run this every week to account for updated data that could potentially arise
+
+# runs the following sequence:
+# 1: web-fetch/checkdate.py // checks dates and downloads necessary pdf data
+# 2: pdf-mod/pdf2txt.py     // does what you think
+# 3: pdf-mod/tts.py         // scrapes the text and sends to appropriate storage facilities (database, json files, etc.)
+
+import os
+import sys
+
+PYTHON_VER = "python3.7"
+
+# get_newest_date: gets the newest date in the dates json list, and returns it
+def get_newest_date():
+    from datetime import datetime
+    import json 
+    dates = None
+    with open("./local-data/dates.json", "r") as fp:
+        dates = json.load(fp)
+        print(dates)
+    
+    keys = []
+    vals = []
+    for key, val in dates.items():
+        date = datetime.strptime(val, "%Y-%m-%d")
+        keys.append(key)
+        vals.append(date)
+    
+    i = vals.index(max(vals))
+    return keys[i] 
+
+# script_glue: combines all the python scripts to run via OS
+def script_glue():
+    date = get_newest_date()
+
+    os.chdir('web-fetch')
+    print("current working directory is: ", os.getcwd())
+    os.system(f'{PYTHON_VER} checkdate.py')
+    os.chdir('../pdf-mod')
+    os.system(f"{PYTHON_VER} pdf2txt.py {date}.pdf > ../local-data/{date}.txt")
+    os.system(f"{PYTHON_VER} tts.py {date}.txt -gd -dj -sd -c")
+
+if __name__ == "__main__":
+    script_glue()
